d320eb5d5ae87530eab67c0c1299a49b84af7bec
Alladio Bonesso <Alladio@users.noreply.github.com>
2022-01-04 19:06:40 -0300

    update

--- minikube.sh
+++ minikube.sh
@@ -13,4 +13,28 @@ sudo apt install virtualbox virtualbox-ext-pack
 
 #WGET MINIKUBE
 wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod 755 /usr/local/bin/minikube
+
+#Kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version -o json
+
+#Step 5: Start Minikube
+minikube start
+kubectl config view
+kubectl cluster-info
+kubectl get nodes
+kubectl get pod
+minikube ssh
+exit
+minikube stop
+
+minikube status
+minikube addons list
+minikube dashboard
+
+minikube dashboard --url
 
---
+++ minikube.sh.bak
@@ -0,0 +1,16 @@
+#minikube
+
+#UPDATE UPGRADE
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#ETC
+sudo apt-get install curl
+sudo apt-get install apt-transport-https
+
+#VIRTUALBOX
+sudo apt install virtualbox virtualbox-ext-pack
+
+#WGET MINIKUBE
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
